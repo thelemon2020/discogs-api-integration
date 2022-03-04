ab09533b8e9ee1a4a3943d98ae97e2a59a0f064e
Vehikl Team <nomorobo-vehikl@gmail.com>
2022-03-04 16:39:58 -0500

    add pagination to master relase version

--- app/Services/Discogs/DiscogsService.php
+++ app/Services/Discogs/DiscogsService.php
@@ -11,9 +11,23 @@ class DiscogsService
     const RELEASES_ENDPOINT = '/releases';
     const MASTERS_ENDPOINT = '/masters';
 
+    private array $queryParams = [];
+
     public function createRequest(): PendingRequest
     {
         return Http::baseUrl(self::BASE_URL);
+    }
+
+    public function page(int $page = 1): DiscogsService
+    {
+        $this->queryParams['page'] = $page;
+        return $this;
+    }
+
+    public function perPage(int $perPage = 25): DiscogsService
+    {
+        $this->queryParams['per_page'] = $perPage;
+        return $this;
     }
 
     public function release(string $releaseId): array
@@ -42,6 +56,11 @@ class DiscogsService
     public function masterReleaseVersions(string $releaseId)
     {
         $url = self::MASTERS_ENDPOINT . '/' . $releaseId . '/versions';
+
+        if($this->queryParams) {
+            $url .= '?' . http_build_query($this->queryParams);
+        }
+
         return $this->createRequest()->get($url)->json();
     }
 }
--- tests/Unit/Services/DiscogsServiceTest.php
+++ tests/Unit/Services/DiscogsServiceTest.php
@@ -87,4 +87,14 @@ class DiscogsServiceTest extends TestCase
         $response = $service->masterReleaseVersions('test');
         $this->assertEquals(['foo' => 'bar'], $response);
     }
+
+    /** @test */
+    public function itCanGetMasterReleaseVersionsWithPagination(): void
+    {
+        $endpoint = DiscogsService::BASE_URL . DiscogsService::MASTERS_ENDPOINT . '/test/versions?page=2&per_page=8';
+        Http::fake([$endpoint => Http::response(['foo' => 'bar'], 200)]);
+        $service = app()->make(DiscogsService::class);
+        $response = $service->page(2)->perPage(8)->masterReleaseVersions('test');
+        $this->assertEquals(['foo' => 'bar'], $response);
+    }
 }
